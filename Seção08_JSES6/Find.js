8be739502596f674f7401981b9e1dc70f0ae4481
suzana <suzana@adxspace.com.br>
2023-05-26 17:34:16 -0300

    usando metodo find com arrow function

--- Seção08_JSES6/Find.js
+++ Seção08_JSES6/Find.js
@@ -1,5 +1,28 @@
 //Método find, encontrar um registro dentro de um array: 
 
-const adservers = ['brasil', 'ahead','space','adforce']
+const twisted_fate = {
+    nome: "Tobias Felix",
+    poder_de_habilidade: 76,
+    qtd_skins: 10,
+    maestria: true
+}
 
-adservers.find(adserver => adserver.adservers === 'space')
+const jinx = {
+    nome: "Powder",
+    poder_de_habilidade: 90,
+    qtd_skins: 12,
+    maestria: false
+}
+
+const kaisa = {
+    nome: "Kaisa Filha do Vazio",
+    poder_de_habilidade: 80,
+    qtd_skins: 5,
+    maestria: true
+}
+
+const wild_rift_champions = [jinx,twisted_fate,kaisa]
+const championsMaestria = wild_rift_champions.find(champ => champ.maestria === true)
+const poderDeHabilidadeAlto = wild_rift_champions.find(champ => champ.poder_de_habilidade > 85)
+console.log(championsMaestria)
+console.log(poderDeHabilidadeAlto)
