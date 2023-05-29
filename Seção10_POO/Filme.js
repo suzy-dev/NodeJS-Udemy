a88517052cba464dece80e0040895d19e5a50ea3
suzana <suzana@adxspace.com.br>
2023-05-29 16:42:33 -0300

    Criando o objeto vingadores da classe filme

--- Seção10_POO/Filme.js
+++ Seção10_POO/Filme.js
@@ -2,20 +2,20 @@
 // metodos: reproduzir, pausar, avançar, fechar. 
 // Boa prática: todo file classe deve começar com letra maiuscula
 // dentro do constructor se define os atributos
-
+//instancia === objeto
 class Filme{
     constructor(){
         this.titulo = ''
         this.ano = 0
-        this.duracao = 0
+        this.duracaoMin = 0
         this.sinopse = ''
         this.diretor = ''
-        this.genero = ''
+        this.genero = []
         this.elenco = []
     }
     
     Reproduzir(){
-        console.log(`Reproduzindo o filme ${this.Filme.titulo}`)
+        console.log(`Reproduzindo o filme. . .`)
     }
 
     Pausar(){
@@ -30,3 +30,22 @@ class Filme{
         console.log(`Fechando o filme XX`)
     }
 }
+
+var VingadoresUltimato = new Filme()
+var StarWarsVingancaSith = new Filme()
+
+VingadoresUltimato.Reproduzir()
+StarWarsVingancaSith.Avançar()
+
+VingadoresUltimato = {
+    titulo: "Vingadores: Ultimato",
+    ano: 2019 , 
+    duracaoMin: 180,
+    sinopse: "After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.", 
+    diretor: "Joe Russo",
+    genero: ['Ação','Ficção Científica','Fantasia'],
+    elenco: ['Chris Hemsworth','Chris Evans','Robert Downey Jr.','Scarlett Johansson']
+}
+console.log(VingadoresUltimato)
+
+
---
+++ Seção10_POO/ObjetoXclasse.js
@@ -0,0 +1,16 @@
+// classe: filme; objeto: Vingadores
+// classe: comida; objeto: Bolo de milho
+// classe: tecnologia: objeto: programação
+// classe: Jogo; objeto: The last of us
+// classe é um modelo para a criação do objeto
+// Uma classe não define os dados, apenas o modelo
+
+class Programação {
+    constructor(){
+        this.linguagem = 
+        this.criador
+        this.tipagem
+    }
+}
+
+const Javascript = new Programação()
