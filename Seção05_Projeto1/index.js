2a72c968d58834c757253d04083d1768b696f01a
suzana-space <suzana@adxspace.com.br>
2023-06-21 17:18:58 -0300

    bodyParser config + sequelize installations

--- Seção05_Projeto1/index.js
+++ Seção05_Projeto1/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('viw engine','ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get("/home",(req, res)=>{
     res.render('index.ejs')
@@ -13,7 +16,9 @@ app.get("/perguntas", (req, res)=>{
 })
 
 app.post("/saveperguntas", (req, res)=>{
-    res.send("formulario recebido")
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+    res.send("Encontre um velho rico ou vire tiktoker")
 })
 
 app.listen(8080,()=>{
