b4edb5d377eb4d9daab37c1da5b3f4fc57e742a5
suzana-space <suzana@adxspace.com.br>
2023-06-22 19:18:36 -0300

    ajustes

--- Seção05_Projeto1/index.js
+++ Seção05_Projeto1/index.js
@@ -23,19 +23,26 @@ app.use(bodyParser.json())
 //Routes:
 app.get("/home",(req, res)=>{
     Pergunta.findAll({raw: true, order:[['id','DESC']]}).then((perguntas)=>{  //DESC - decrescente, ASC - crescente
-        res.render('index.ejs', { perguntas: perguntas})
+        res.render('home.ejs', { perguntas: perguntas})
     })
     
 })
 
-app.get("/perguntas", (req, res)=>{
-    res.render('perguntas.ejs')
+app.get("/perguntar", (req, res)=>{
+    res.render('perguntar.ejs')
 })
 
 app.post("/saveperguntas", (req, res)=>{
     let titulo = req.body.titulo
     let descricao = req.body.descricao
-    Pergunta.create({titulo: titulo, descricao: descricao}).then(()=>{res.redirect("/home")})
+    Pergunta.create({titulo: titulo, descricao: descricao}).then(() => {res.redirect("/home")})
+})
+
+app.get("/home/:id",(req, res)=>{
+    let id = req.params.id
+    Pergunta.findOne({ where: {id: id} }).then((perguntas) => {
+        perguntas != undefined ? res.render("pergunta.ejs") : res.redirect("/home")
+    })
 })
 
 app.listen(8080,()=>{
