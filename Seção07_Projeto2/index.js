4aead886a3b860813fa1baa6431053819dd30afb
suzana-space <suzana@adxspace.com.br>
2023-07-03 14:03:59 -0300

    import modelsControllers and Models

--- Seção07_Projeto2/index.js
+++ Seção07_Projeto2/index.js
@@ -3,8 +3,13 @@ import bodyParser from 'body-parser'
 import connection from './database/database.js'
 const porta = 8080
 const app = express()
+
 import articlesController from './articles/articlesController.js'
 import categoriesController from './categories/categoriesController.js'
+
+import Category from './categories/Category.js'
+import Article from './articles/Article.js'
+
 // View Engine EJS
 app.set('view engine', 'ejs')
 
@@ -18,7 +23,7 @@ app.use(bodyParser.json())
 // Database
 connection
 .authenticate()
-.then(() => {console.log("sucessful")})
+.then(() => {console.log("connection database sucessful")})
 .catch((error) => {console.log(error)})
 
 app.get("/", (req, res) => {
@@ -29,5 +34,5 @@ app.use("/", categoriesController)
 app.use("/", articlesController)
 
 app.listen(porta, () => {
-    console.log(`server openen in localhost:${porta}/`)
+    console.log(`server opened in localhost:${porta}/`)
 })
