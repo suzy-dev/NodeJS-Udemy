c98cf278017b78d593c8ed60dc3d1dff5e6df4a5
suzana-space <suzana@adxspace.com.br>
2023-07-06 12:42:53 -0300

    form new category + CDN bootstrap + navbar

--- Seção07_Projeto2/categories/categoriesController.js
+++ Seção07_Projeto2/categories/categoriesController.js
@@ -1,8 +1,25 @@
 import express from 'express'
+import Category from './Category.js'
+import slugify from 'slugify'
 const router = express.Router()
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/categories.ejs")
+})
+
+
+router.post("/categoriesnew/save", (req, res) => {
+    let titleCategory = req.body.titleCategory
+    if (titleCategory != undefined){
+        Category.create({title: titleCategory}, 
+                        {slug: slugify(title)})
+        .then(() => {
+            res.redirect("/")
+        })
+    }
+    else {
+        res.redirect("/admin/categories/new")
+    }
 })
 
 export default router
