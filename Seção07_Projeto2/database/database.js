c54f31f0ab8b317dad421bc29aad9bd8c50a43f0
suzana-space <suzana@adxspace.com.br>
2023-07-13 18:05:40 -0300

    ajuste do UTC do sequelize para -03:00

--- Seção07_Projeto2/database/database.js
+++ Seção07_Projeto2/database/database.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize'
 
 const connection = new Sequelize('blog', 'root', 'susu1313',{
-    host: 'localhost',
-    dialect: 'mysql'
+    host     : 'localhost',
+    dialect  : 'mysql',
+    timezone : '-03:00' 
 })
 
 export default connection
