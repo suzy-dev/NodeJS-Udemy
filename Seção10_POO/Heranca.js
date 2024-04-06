32bdcdbf965ba61fa6ad766e6f31adfaef5f0d73
suzana souza <suzanasouza.caetano@gmail.com>
2024-04-06 13:58:57 -0300

    hernaca, super()

--- Seção10_POO/Heranca.js
+++ Seção10_POO/Heranca.js
@@ -1,2 +1,22 @@
-// reutilizar código
+// ter uma classe mae e uma classe filha / reutilizar código
+class Animal {
+
+    constructor(nome,idade,raca,preco){
+        this.nome = nome
+        this.idade = idade
+        this.raca = raca
+        this.preco = preco
+    }
+
+    checarEstoque(){
+        return 10
+    }
+}
+
+class Cachorro extends Animal {
+
+}
+
+const dog = new Cachorro("laila",15,"pinscher",120)
+console.log(dog)
 
